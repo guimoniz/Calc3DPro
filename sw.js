d5f9ecf9488e1c179e0375486496787d71830ba2
guimoniz <56410357+guimoniz@users.noreply.github.com>
2026-04-26 22:33:28 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='calc3d-v1';
+const ASSETS=['./','./index.html','./icon.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim());});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
