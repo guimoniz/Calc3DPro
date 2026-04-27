280a4c470f5adad3bef134c3a917d17051685502
guimoniz <56410357+guimoniz@users.noreply.github.com>
2026-04-26 23:35:01 -0300

    sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='calc3d-v2';
+const CACHE='calc3d-v1.2';
 const ASSETS=['./','./index.html','./icon.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim());});
